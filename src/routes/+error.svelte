c70fd6fe43f43e41bbaa4879d5d8408db31da4e3
Kevin-Moreau65 <kevin.moreau655@gmail.com>
2025-01-24 09:57:42 +0100

    test

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+    import { page } from '$app/stores';
 </script>
 
 <div class="flex w-full flex-col items-center gap-40 pt-10">
-	<h1 class="text-7xl">{$page.status}</h1>
-	<h2 class="text-5xl">{$page.error?.message}</h2>
+    <h1 class="text-7xl">{$page.status}</h1>
+    <h2 class="text-5xl">{$page.error?.message}</h2>
 </div>
