3a53d38f25021ee3f04f0b787bb813cf5cf71f47
Kevin-Moreau65 <kevin.moreau655@gmail.com>
2025-01-27 13:15:14 +0100

    Update User.spec.ts

--- tests/unit/models/User.spec.ts
+++ tests/unit/models/User.spec.ts
@@ -2,12 +2,12 @@ import User from '$lib/models/user';
 import { expect, test } from 'vitest';
 
 test.each([
-	{ password: 'K@u1', expected: false },
-	{ password: 'aaaaa@u1', expected: false },
-	{ password: 'PPPPPP@1', expected: false },
-	{ password: 'P@uuuuuu', expected: false },
-	{ password: 'KKKajfu1', expected: false },
-	{ password: 'Pa$$word8', expected: true }
+    { password: 'K@u1', expected: false },
+    { password: 'aaaaa@u1', expected: false },
+    { password: 'PPPPPP@1', expected: false },
+    { password: 'P@uuuuua', expected: false },
+    { password: 'KKKajfu1', expected: false },
+    { password: 'Pa$$word8', expected: true }
 ])('password $password must be $expected', ({ password, expected }) => {
-	expect(User.shape.password.safeParse(password).success).toBe(expected);
+    expect(User.shape.password.safeParse(password).success).toBe(expected);
 });
